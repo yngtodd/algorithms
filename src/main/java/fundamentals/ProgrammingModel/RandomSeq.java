1a06ebc98ad031b68c4ee604b4081e57b408b312
yngtodd <young.todd.mk@gmail.com>
2018-12-04 17:14:58 -0500

    Adding random sequence bits

---
+++ src/main/java/fundamentals/ProgrammingModel/RandomSeq.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+ *  Compilation:  javac RandomSeq.java
+ *  Execution:    java RandomSeq n lo hi
+ *  Dependencies: StdOut.java, StdRandom.java
+ *
+ *  Prints N numbers between lo and hi.
+ *
+ *  % java RandomSeq 5 100.0 200.0
+ *  123.43
+ *  153.13
+ *  144.38
+ *  155.18
+ *  104.02
+ *
+ ******************************************************************************/
+
+import edu.princeton.cs.algs4.StdOut
+import edu.princeton.cs.algs4.StdRandom
+
+public class RandomSeq
+{
+    private RandomSeq() { }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
