e69a461ae0349fef69eb151e95984f8455f514de
yngtodd <young.todd.mk@gmail.com>
2018-12-04 18:36:50 -0500

    Random numbers

--- src/main/java/fundamentals/ProgrammingModel/RandomSeq.java
+++ src/main/java/fundamentals/ProgrammingModel/RandomSeq.java
@@ -14,8 +14,8 @@
  *
  ******************************************************************************/
 
-import edu.princeton.cs.algs4.StdOut
-import edu.princeton.cs.algs4.StdRandom
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomSeq
 {
@@ -23,6 +23,29 @@ public class RandomSeq
 
     public static void main(String[] args)
     {
+        int n = Integer.parseInt(args[0]);
 
+        if (args.length == 1)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                double x = StdRandom.uniform();
+                StdOut.println(x);
+            }
+        }
+
+        else if (args.length == 3)
+        {
+            double lo = Double.parseDouble(args[1]);
+            double hi = Double.parseDouble(args[2]);
+
+            for (int i = 0; i < n; i++)
+            {
+                double x = StdRandom.uniform(lo, hi);
+                StdOut.printf("%.2f\n", x);
+            }
+        }
+
+        else { throw new IllegalArgumentException("Invalid number of arguments."); }
     }
 }
