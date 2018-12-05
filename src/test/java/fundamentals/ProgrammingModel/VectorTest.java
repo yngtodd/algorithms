6dcdc35c6ec27756e7210960482402626ca59db3
yngtodd <young.todd.mk@gmail.com>
2018-12-04 22:00:05 -0500

    move dirs

---
+++ src/test/java/fundamentals/ProgrammingModel/VectorTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import edu.princeton.cs.algs4.StdOut; 
+
+public class VectorTest
+{
+    @Test public void Vector()
+    {
+        double[] xdata = { 1.0, 2.0, 3.0, 4.0 };
+        double[] ydata = { 5.0, 2.0, 4.0, 1.0 };
+
+        Vector x = new Vector(xdata);
+        Vector y = new Vector(ydata);
+
+        StdOut.println("   x       = " + x);
+        StdOut.println("   y       = " + y);
+
+        Vector z = x.plus(y);
+        StdOut.println("   z       = " + z);
+
+        z = z.times(10.0);
+        StdOut.println(" 10z       = " + z);
+
+        StdOut.println("  |x|      = " + x.magnitude());
+        StdOut.println(" <x, y>    = " + x.dot(y));
+        StdOut.println("dist(x, y) = " + x.distanceTo(y));
+        StdOut.println("dir(x)     = " + x.direction());
+    }
+}
